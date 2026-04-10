de8add18ef5a1eabd3865b69d30cf882404effc3
deewau <skvortsov3401@gmail.com>
2026-04-10 18:22:19 +0300

    refactor: extract icon components to separate files

---
+++ src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { TrophyIcon } from './TrophyIcon';
+export { GiftsIcon } from './GiftsIcon';
+export { GameIcon } from './GameIcon';
+export { TasksIcon } from './TasksIcon';
+export { ProfileIcon } from './ProfileIcon';
