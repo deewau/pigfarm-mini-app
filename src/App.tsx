047085586f991202fe2d5c35c62b369c64ca37a2
deewau <skvortsov3401@gmail.com>
2026-04-10 17:26:21 +0300

    init: Telegram Mini App

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: 40 }}>
+      <h1>✅ Mini App работает!</h1>
+      <p>Telegram WebApp готов</p>
+    </div>
+  )
+}
+
+export default App
