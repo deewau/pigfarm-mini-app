0c5cbdf53b0f1e1bd86615551e80c3785a435766
deewau <skvortsov3401@gmail.com>
2026-04-10 17:34:39 +0300

    fix: use native Telegram WebApp SDK

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import WebApp from '@twa-dev/sdk'
 
-WebApp.ready()
+// Инициализация Telegram WebApp
+const tg = (window as any).Telegram?.WebApp
+if (tg) {
+  tg.ready()
+  tg.expand() // Развернуть на весь экран
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
